868358cb8e84440cfc628bac8584bef8ef691cc1
Vladislav Ilin <ilin96inbox@gmail.com>
2020-07-17 18:35:55 +0900

    add data

--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -6,8 +6,19 @@ import {
   IonTitle,
   IonButton,
   IonPage,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent,
 } from "@ionic/react";
 import { useHistory } from "react-router";
+
+const COURSE_DATA = [
+  { id: "c1", title: "Ionic + React" },
+  { id: "c2", title: "Java" },
+  { id: "c3", title: "C++" },
+];
 
 const Courses: React.FC = () => {
   const history = useHistory();
@@ -22,13 +33,20 @@ const Courses: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <h2>This works</h2>
-        <div>
-          <IonButton routerLink="/course-goals">To course goals</IonButton>
-          {/* <IonButton onClick={changePageHandler} routerLink="/course-goals">
-            To course goals
-          </IonButton> */}
-        </div>
+        <IonGrid>
+          {COURSE_DATA.map((course) => {
+            <IonRow key={course.id}>
+              <IonCol size-md="4" offset-md="4">
+                <IonCard>
+                  <IonCardContent>
+                    <h2 className="ion-text-center">{course.title}</h2>
+                    <IonButton routerLink="">View Course Goals</IonButton>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>;
+          })}
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
